64727a78fb6d10d599965c270ad84f9090d805f8
dalu-zhanglu <535661173@qq.com>
2017-10-30 09:59:10 +0800

    导出货道二维码

--- yxkj-pom/yxkj-operation/src/main/java/com/yxkj/controller/file/FileController.java
+++ yxkj-pom/yxkj-operation/src/main/java/com/yxkj/controller/file/FileController.java
@@ -1,11 +1,16 @@
 package com.yxkj.controller.file;
 
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executor;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +21,23 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.yxkj.beans.CommonAttributes;
 import com.yxkj.controller.base.BaseController;
+import com.yxkj.entity.Company;
+import com.yxkj.entity.ContainerChannel;
+import com.yxkj.entity.Goods;
+import com.yxkj.entity.VendingContainer;
 import com.yxkj.entity.commonenum.CommonEnum;
 import com.yxkj.entity.commonenum.CommonEnum.ImageType;
+import com.yxkj.json.base.BaseRequest;
 import com.yxkj.json.base.BaseResponse;
 import com.yxkj.json.base.ResponseOne;
 import com.yxkj.service.AdResourceService;
+import com.yxkj.service.ContainerChannelService;
 import com.yxkj.service.FileService;
+import com.yxkj.service.VendingContainerService;
+import com.yxkj.common.log.LogUtil;
+import com.yxkj.utils.FieldFilterUtils;
+import com.yxkj.utils.GeneratePdf;
+import com.yxkj.utils.TimeUtils;
 
 import io.swagger.annotations.Api;
 
@@ -38,8 +54,18 @@ public class FileController extends BaseController {
 
   @Resource(name = "fileServiceImpl")
   private FileService fileService;
+  
   @Resource(name = "adResourceServiceImpl")
   private AdResourceService adResourceService;
+  
+  @Resource(name = "vendingContainerServiceImpl")
+  private VendingContainerService vendingContainerService;
+  
+  @Resource(name = "containerChannelServiceImpl")
+  private ContainerChannelService containerChannelService;
+  
+  @Resource(name = "taskExecutor")
+  private Executor threadPoolExecutor;
 
   /**
    * 上传文件(单个)
@@ -87,5 +113,60 @@ public class FileController extends BaseController {
     }
     response.setCode(CommonAttributes.SUCCESS);
     return response;
+  }
+  
+  /**
+   * 导出某个货柜所选货道二维码pdf
+   * 
+   */
+  @RequestMapping(value = "/downloadQrPdf", method = {RequestMethod.GET, RequestMethod.POST})
+  public void downloadQrPdf(BaseRequest request, HttpServletResponse response,
+      HttpSession session) {
+    if (request.getId() == null || request.getIds() == null ||  request.getIds().length == 0) {
+    	LogUtil.debug(this.getClass(), "downloadQrPdf", "货柜ID为空");
+    	return;
+	}
+    if (request.getIds() == null ||  request.getIds().length == 0 ) {
+    	LogUtil.debug(this.getClass(), "downloadQrPdf", "货道列表为空");
+    	return;
+	}
+    VendingContainer container = vendingContainerService.find(request.getId());
+    Long sceneId = container.getScene().getId();
+    String sceneName = container.getScene().getName();
+    Long containerId = container.getId();
+    String containerSn = container.getSn();
+    String title = sceneName + containerSn;
+    
+    List<ContainerChannel> channels =  containerChannelService.findList(request.getIds());
+    String[] propertys = {"id", "sn"};
+    List<Map<String, Object>> channelList = FieldFilterUtils.filterCollection(propertys, channels);
+
+    try {
+      response.setContentType("octets/stream");
+      String filename = TimeUtils.getDateFormatString("yyyyMMddHHmmss", new Date());
+      response.addHeader("Content-Disposition", "attachment;filename=" + filename + ".pdf");
+
+      OutputStream out = response.getOutputStream();// 获得输出流
+      GeneratePdf generatePdf = new GeneratePdf(sceneId, containerId, title, channelList, out, fileService.getQrCodePrefixUrl());
+      Object locker = new Object();// 当前主线程的一把锁
+      synchronized (locker) {
+        threadPoolExecutor.execute(// 加入到线程池中执行
+            new Runnable() {
+              public void run() {
+                generatePdf.generatePdf();
+                synchronized (locker) {
+                  locker.notify();
+                }
+              }
+            });
+        locker.wait();// 主线程等待
+      }
+
+      out.flush();
+      out.close();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
---
+++ yxkj-pom/yxkj-operation/src/main/java/com/yxkj/utils/QRCodeGenerator.java
@@ -0,0 +1,77 @@
+package com.yxkj.utils;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+public class QRCodeGenerator {
+
+  public QRCodeGenerator() {
+
+  }
+
+  public byte[] generateQrImage(String uuid) {
+    int size = 88;
+    byte[] bytes = null;
+    try {
+      Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap =
+          new Hashtable<EncodeHintType, ErrorCorrectionLevel>();
+      hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);
+      QRCodeWriter qrCodeWriter = new QRCodeWriter();
+      BitMatrix byteMatrix = qrCodeWriter.encode(uuid, BarcodeFormat.QR_CODE, size, size, hintMap);
+      int CrunchifyWidth = byteMatrix.getWidth();
+      CrunchifyWidth = CrunchifyWidth-22;
+      System.out.println(CrunchifyWidth);
+      BufferedImage image =
+          new BufferedImage(CrunchifyWidth, CrunchifyWidth, BufferedImage.TYPE_INT_RGB);
+      image.createGraphics();
+
+      Graphics2D graphics = (Graphics2D) image.getGraphics();
+      graphics.setColor(Color.WHITE);
+      graphics.fillRect(0, 0, CrunchifyWidth, CrunchifyWidth);
+      graphics.setColor(Color.BLACK);
+
+      for (int i = 0; i < CrunchifyWidth; i++) {
+        for (int j = 0; j < CrunchifyWidth; j++) {
+          if (byteMatrix.get(i+11, j+11)) {
+            graphics.fillRect(i, j, 1, 1);
+          }
+        }
+      }
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      ImageIO.write(image, "jpg", baos);
+      baos.flush();
+      bytes = baos.toByteArray();
+      baos.close();
+      return bytes;
+    } catch (WriterException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return bytes;
+  }
+
+  public static void main(String[] args) {
+    // byte[] bytes = generateQrImage ("http://carlife.chcws.com:8080/upload/apk/csh.apk");
+    String content = "{\"companySn\":\"10000000000001" + "\",\"goodsSn\":\"10000000004" + "\"}";
+    QRCodeGenerator qrCodeGenerator = new QRCodeGenerator();
+    byte[] bytes = qrCodeGenerator.generateQrImage(content);
+
+    System.out.println(bytes.length);
+
+  }
+}
