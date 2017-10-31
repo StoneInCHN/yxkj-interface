ada3c754b64bb152d95912a2b83325c4efdbb972
huyong <82370288@qq.com>
2017-10-31 14:43:01 +0800

    增加出货测试，音量调节，重启系统指令接口

---
+++ yxkj-pom/yxkj-app/src/main/java/com/yxkj/controller/keeper/CmdController.java
@@ -0,0 +1,77 @@
+package com.yxkj.controller.keeper;
+
+import javax.annotation.Resource;
+
+import io.swagger.annotations.ApiParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.yxkj.beans.CommonAttributes;
+import com.yxkj.controller.base.MobileBaseController;
+import com.yxkj.json.base.BaseResponse;
+import com.yxkj.json.request.CmdRequest;
+import com.yxkj.service.CmdService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Controller("CmdController")
+@RequestMapping("/cmd")
+public class CmdController extends MobileBaseController {
+
+  @Resource(name = "cmdServiceImpl")
+  private CmdService cmdService;
+
+  /**
+   * 测试出货
+   */
+  @RequestMapping("salesOutTest")
+  @ApiOperation(value = "salesOutTest", httpMethod = "POST", response = BaseResponse.class,
+      notes = "salesOutTest", tags = "CmdController")
+  @ApiResponses({
+      @ApiResponse(code = 200, message = "code:0000-request success|code:1000-auth fail")})
+  @ResponseBody
+  public BaseResponse salesOutTest(@ApiParam(name = "请求参数(json)", value = "channelId:货道ID",
+      required = true)@RequestBody CmdRequest request) {
+    BaseResponse response = new BaseResponse();
+    cmdService.salesOutTest(request.getChannelId());
+    response.setCode(CommonAttributes.SUCCESS);
+    return response;
+  }
+
+  /**
+   * 调整音量
+   */
+  @RequestMapping("updateAudioVolume")
+  @ApiOperation(value = "updateAudioVolume", httpMethod = "POST", response = BaseResponse.class,
+      notes = "updateAudioVolume", tags = "CmdController")
+  @ApiResponses({
+      @ApiResponse(code = 200, message = "code:0000-request success|code:1000-auth fail")})
+  @ResponseBody
+  public BaseResponse updateAudioVolume(@ApiParam(name = "请求参数(json)", value = "deviceNo:设备号；volume:音量（0-100）",
+      required = true) @RequestBody CmdRequest request) {
+    BaseResponse response = new BaseResponse();
+    cmdService.updateAudioVolume(request.getDeviceNo(), Float.parseFloat(request.getVolume()));
+    response.setCode(CommonAttributes.SUCCESS);
+    return response;
+  }
+  /**
+   * 重启设备
+   */
+  @RequestMapping("rebootSystem")
+  @ApiOperation(value = "rebootSystem", httpMethod = "POST", response = BaseResponse.class,
+      notes = "rebootSystem", tags = "CmdController")
+  @ApiResponses({
+      @ApiResponse(code = 200, message = "code:0000-request success|code:1000-auth fail")})
+  @ResponseBody
+  public BaseResponse rebootSystem(@ApiParam(name = "请求参数(json)", value = "deviceNo:设备号",
+      required = true)@RequestBody CmdRequest request) {
+    BaseResponse response = new BaseResponse();
+    cmdService.appReboot(request.getDeviceNo());
+    response.setCode(CommonAttributes.SUCCESS);
+    return response;
+  }
+}
---
+++ yxkj-pom/yxkj-app/src/main/java/com/yxkj/service/impl/CmdServiceImpl.java
@@ -0,0 +1,98 @@
+package com.yxkj.service.impl;
+
+import java.io.IOException;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yxkj.common.client.ReceiverClient;
+import com.yxkj.common.commonenum.CommonEnum;
+import com.yxkj.common.entity.CmdMsg;
+import com.yxkj.common.log.LogUtil;
+import com.yxkj.dao.CommandRecordDao;
+import com.yxkj.entity.CommandRecord;
+import com.yxkj.entity.ContainerChannel;
+import com.yxkj.entity.VendingContainer;
+import com.yxkj.framework.service.impl.BaseServiceImpl;
+import com.yxkj.service.CmdService;
+import com.yxkj.service.ContainerChannelService;
+import com.yxkj.service.OrderService;
+
+/**
+ * @author huyong
+ * @since 2017/9/26
+ */
+@Service("cmdServiceImpl")
+public class CmdServiceImpl extends BaseServiceImpl<CommandRecord, Long> implements CmdService {
+
+  private CommandRecordDao commandRecordDao;
+
+  @Resource(name = "containerChannelServiceImpl")
+  private ContainerChannelService containerChannelService;
+  private ReceiverClient receiverClient;
+
+  @Autowired
+  public CmdServiceImpl(ReceiverClient receiverClient, CommandRecordDao commandRecordDao) {
+    this.commandRecordDao = commandRecordDao;
+    this.receiverClient = receiverClient;
+  }
+
+  private Long saveCommandRecord(String deviceNo, CommonEnum.CmdType cmdType, String content) {
+    CommandRecord record = new CommandRecord();
+
+    // 保存指令到数据库
+    record.setCmdContent(content);
+    record.setCmdType(cmdType);
+    record.setDeviceNo(deviceNo);
+    record.setCmdStatus(CommonEnum.CmdStatus.SendOut);
+    commandRecordDao.persist(record);
+
+    return record.getId();
+  }
+
+  @Override
+  public void salesOutTest(Long channelId) {
+
+    ContainerChannel containerChannel = containerChannelService.find(channelId);
+    VendingContainer vendingContainer = containerChannel.getCntr().getParent();
+    String deviceNo = vendingContainer.getSn();
+    String address = containerChannel.getCntr().getSn();
+    int addressType = containerChannel.getCntr().getCategory().getCntrType().ordinal();
+    StringBuilder content = new StringBuilder();
+    content.append("channelId:");
+    content.append(channelId);
+    Long recordId = saveCommandRecord(deviceNo, CommonEnum.CmdType.SELL_OUT, content.toString());
+    try {
+      receiverClient.sellOutTest(deviceNo, recordId, address, addressType,
+          containerChannel.getSn());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+
+  @Override
+  public void updateAudioVolume(String deviceNo, float volume) {
+    try {
+      Long recordId =
+          saveCommandRecord(deviceNo, CommonEnum.CmdType.VOLUME, String.valueOf(volume));
+      CmdMsg cmdMsg = receiverClient.updateAudioVolume(deviceNo, volume, recordId);
+      LogUtil.debug(this.getClass(), "updateAudioVolume", "CmdMsg = %s", cmdMsg.toString());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void appReboot(String deviceNo) {
+    try {
+      Long recordId = saveCommandRecord(deviceNo, CommonEnum.CmdType.VOLUME, null);
+      CmdMsg cmdMsg = receiverClient.reboot(deviceNo, recordId);
+      LogUtil.debug(this.getClass(), "updateAudioVolume", "CmdMsg = %s", cmdMsg.toString());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
